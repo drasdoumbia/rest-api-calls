f47b2efa5bee20791c30be337c3efa0454095ca5
dramanesd <ddramane63@gmail.com>
2021-02-03 13:21:11 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/dramanedoumbia/rest_api_calls/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dramanedoumbia.rest_api_calls
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Api Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text("API practices!"),
+          ),
+        ),
+      ),
+    );
+  }
+}
